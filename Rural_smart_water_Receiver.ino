c85edab9019aa53495b05aa9f5f71d1103779d61
Temalangeni Minenhle Dlamini <152439176+minenhledlamini12@users.noreply.github.com>
2024-03-11 00:48:49 +0200

    Add files via upload
    second board code that receives and upload data to firebase

---
+++ Rural_smart_water_Receiver.ino
@@ -0,0 +1,462 @@
+/*********
+  firebase uploader code 
+*********/
+
+// Import Wi-Fi library
+#include <WiFi.h>
+#include "ESPAsyncWebServer.h"
+
+#include <SPIFFS.h>
+
+//Libraries for LoRa
+#include <SPI.h>
+#include <LoRa.h>
+
+//Libraries for OLED Display
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+// Libraries to get time from NTP Server
+#include <NTPClient.h>
+#include <WiFiUdp.h>
+
+#include <Arduino.h>
+#if defined(ESP32)
+  #include <WiFi.h>
+#elif defined(ESP8266)
+  #include <ESP8266WiFi.h>
+#endif
+#include <Firebase_ESP_Client.h>
+#include <Wire.h>
+#include <Adafruit_Sensor.h>
+
+//define the pins used by the LoRa transceiver module
+#define SCK 5
+#define MISO 19
+#define MOSI 27
+#define SS 18
+#define RST 14
+#define DIO0 26
+
+//433E6 for Asia
+//866E6 for Europe
+//915E6 for North America
+#define BAND 866E6
+
+//OLED pins
+#define OLED_SDA 4
+#define OLED_SCL 15 
+#define OLED_RST 16
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 64 // OLED display height, in pixels
+
+// Replace with your network credentials
+const char* ssid     = "MTN_4G_B8EB";
+const char* password = "30483891";
+
+// Define NTP Client to get time
+WiFiUDP ntpUDP;
+NTPClient timeClient(ntpUDP);
+
+// Variables to save date and time
+String formattedDate;
+String day;
+String hour;
+String timestamp;
+
+
+// Initialize variables to get and save LoRa data
+int rssi;
+String loRaMessage;
+String temperature;
+String humidity;
+String pressure;
+String readingID;
+
+
+// Provide the token generation process info.
+#include "addons/TokenHelper.h"
+// Provide the RTDB payload printing info and other helper functions.
+#include "addons/RTDBHelper.h"
+
+// Insert your network credentials
+#define WIFI_SSID "MTN_4G_B8EB"
+#define WIFI_PASSWORD "30483891"
+
+// Insert Firebase project API Key
+#define API_KEY "AIzaSyCSF_RI5mHbmgcDEPvZHJQ6HRaazeDVros"
+
+// Insert Authorized Username and Corresponding Password
+#define USER_EMAIL "dminenhle477@gmail.com"
+#define USER_PASSWORD "1234567890"
+
+
+// Create AsyncWebServer object on port 80
+AsyncWebServer server(80);
+
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RST);
+
+// Define Firebase objects
+FirebaseData fbdo;
+FirebaseData stream;
+FirebaseAuth auth;
+FirebaseConfig config;
+
+// Variable to save USER UID
+String uid;
+// Variables to save database paths
+String databasePath;
+String tempPath;
+String humPath;
+String presPath;
+
+// Replaces placeholder with DHT values
+String processor(const String& var){
+  //Serial.println(var);
+  if(var == "Turbidity"){
+    return turbidity;
+  }
+  if(var == "TEMPERATURE"){
+    return tdsValue;
+  }
+  if(var == "pH_val"){
+    return pH_Value;
+  }
+  else if(var == "STATE1"){
+    return output1;
+  }
+  else if(var == "STATE2"){
+    return output2;
+  }
+  else if(var == "STATE3"){
+    return output3;
+  }
+  else if (var == "RRSI"){
+    return String(rssi);
+  }
+  return String();
+}
+
+//Initialize OLED display
+void startOLED(){
+  //reset OLED display via software
+  pinMode(OLED_RST, OUTPUT);
+  digitalWrite(OLED_RST, LOW);
+  delay(20);
+  digitalWrite(OLED_RST, HIGH);
+
+  //initialize OLED
+  Wire.begin(OLED_SDA, OLED_SCL);
+  if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3c, false, false)) { // Address 0x3C for 128x32
+    Serial.println(F("SSD1306 allocation failed"));
+    for(;;); // Don't proceed, loop forever
+  }
+  display.clearDisplay();
+  display.setTextColor(WHITE);
+  display.setTextSize(1);
+  display.setCursor(0,0);
+  display.print("LORA SENDER");
+}
+
+//Initialize LoRa module
+void startLoRA(){
+  int counter;
+  //SPI LoRa pins
+  SPI.begin(SCK, MISO, MOSI, SS);
+  //setup LoRa transceiver module
+  LoRa.setPins(SS, RST, DIO0);
+
+  while (!LoRa.begin(BAND) && counter < 10) {
+    Serial.print(".");
+    counter++;
+    delay(500);
+  }
+  if (counter == 10) {
+    // Increment readingID on every new reading
+    Serial.println("Starting LoRa failed!"); 
+  }
+  Serial.println("LoRa Initialization OK!");
+  display.setCursor(0,10);
+  display.clearDisplay();
+  display.print("LoRa Initializing OK!");
+  display.display();
+  delay(2000);
+}
+
+void connectWiFi(){
+  // Connect to Wi-Fi network with SSID and password
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  // Initialize WiFi
+void initWiFi() {
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  Serial.print("Connecting to WiFi ..");
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print('.');
+    delay(1000);
+  }
+  Serial.println(WiFi.localIP());
+  Serial.println();
+}
+
+// Write float values to the database
+void sendFloat(String path, float value){
+  if (Firebase.RTDB.setFloat(&fbdo, path.c_str(), value)){
+    Serial.print("Writing value: ");
+    Serial.print (value);
+    Serial.print(" on the following path: ");
+    Serial.println(path);
+    Serial.println("PASSED");
+    Serial.println("PATH: " + fbdo.dataPath());
+    Serial.println("TYPE: " + fbdo.dataType());
+  }
+  else {
+    Serial.println("FAILED");
+    Serial.println("REASON: " + fbdo.errorReason());
+  }
+}
+  // Print local IP address and start web server
+  Serial.println("");
+  Serial.println("WiFi connected.");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+  display.setCursor(0,20);
+  display.print("Access web server at: ");
+  display.setCursor(0,30);
+  display.print(WiFi.localIP());
+  display.display();
+}
+
+// Read LoRa packet and get the sensor readings
+void getLoRaData() {
+  Serial.print("Lora packet received: ");
+  // Read packet
+  while (LoRa.available()) {
+    String LoRaData = LoRa.readString();
+    // LoRaData format: readingID/temperature&soilMoisture#batterylevel
+    // String example: 1/27.43&654#95.34
+    Serial.print(LoRaData); 
+    
+    // Get readingID, temperature and soil moisture
+    int pos1 = LoRaData.indexOf('/');
+    int pos2 = LoRaData.indexOf('&');
+    int pos3 = LoRaData.indexOf('#');
+    readingID = LoRaData.substring(0, pos1);
+    temperature = LoRaData.substring(pos1 +1, pos2);
+    humidity = LoRaData.substring(pos2+1, pos3);
+    pressure = LoRaData.substring(pos3+1, LoRaData.length());    
+  }
+  // Get RSSI
+  rssi = LoRa.packetRssi();
+  Serial.print(" with RSSI ");    
+  Serial.println(rssi);
+}
+
+// Function to get date and time from NTPClient
+void getTimeStamp() {
+  while(!timeClient.update()) {
+    timeClient.forceUpdate();
+  }
+  // The formattedDate comes with the following format:
+  // 2018-05-28T16:00:13Z
+  // We need to extract date and time
+  formattedDate = timeClient.getFormattedDate();
+  Serial.println(formattedDate);
+
+  // Extract date
+  int splitT = formattedDate.indexOf("T");
+  day = formattedDate.substring(0, splitT);
+  Serial.println(day);
+  // Extract time
+  hour = formattedDate.substring(splitT+1, formattedDate.length()-1);
+  Serial.println(hour);
+  timestamp = day + " " + hour;
+}
+
+// Callback function that runs on database changes
+void streamCallback(FirebaseStream data){
+  Serial.printf("stream path, %s\nevent path, %s\ndata type, %s\nevent type, %s\n\n",
+                data.streamPath().c_str(),
+                data.dataPath().c_str(),
+                data.dataType().c_str(),
+                data.eventType().c_str());
+  printResult(data); //see addons/RTDBHelper.h
+  Serial.println();
+
+  // Get the path that triggered the function
+  String streamPath = String(data.dataPath());
+
+  // if the data returned is an integer, there was a change on the GPIO state on the following path /{gpio_number}
+  if (data.dataTypeEnum() == fb_esp_rtdb_data_type_integer){
+    String gpio = streamPath.substring(1);
+    int state = data.intData();
+    Serial.print("GPIO: ");
+    Serial.println(gpio);
+    Serial.print("STATE: ");
+    Serial.println(state);
+    digitalWrite(gpio.toInt(), state);
+  }
+
+  /* When it first runs, it is triggered on the root (/) path and returns a JSON with all keys
+  and values of that path. So, we can get all values from the database and updated the GPIO states*/
+  if (data.dataTypeEnum() == fb_esp_rtdb_data_type_json){
+    FirebaseJson json = data.to<FirebaseJson>();
+
+    // To iterate all values in Json object
+    size_t count = json.iteratorBegin();
+    Serial.println("\n---------");
+    for (size_t i = 0; i < count; i++){
+        FirebaseJson::IteratorValue value = json.valueAt(i);
+        int gpio = value.key.toInt();
+        int state = value.value.toInt();
+        Serial.print("STATE: ");
+        Serial.println(state);
+        Serial.print("GPIO:");
+        Serial.println(gpio);
+        digitalWrite(gpio, state);
+        Serial.printf("Name: %s, Value: %s, Type: %s\n", value.key.c_str(), value.value.c_str(), value.type == FirebaseJson::JSON_OBJECT ? "object" : "array");
+    }
+    Serial.println();
+    json.iteratorEnd(); // required for free the used memory in iteration (node data collection)
+  }
+  
+  //This is the size of stream payload received (current and max value)
+  //Max payload size is the payload size under the stream path since the stream connected
+  //and read once and will not update until stream reconnection takes place.
+  //This max value will be zero as no payload received in case of ESP8266 which
+  //BearSSL reserved Rx buffer size is less than the actual stream payload.
+  Serial.printf("Received stream payload size: %d (Max. %d)\n\n", data.payloadLength(), data.maxPayloadLength());
+}
+
+void streamTimeoutCallback(bool timeout){
+  if (timeout)
+    Serial.println("stream timeout, resuming...\n");
+  if (!stream.httpConnected())
+    Serial.printf("error code: %d, reason: %s\n\n", stream.httpCode(), stream.errorReason().c_str());
+}
+
+void setup() { 
+  // Initialize Serial Monitor
+  Serial.begin(115200);
+  startOLED();
+  startLoRA();
+  connectWiFi();
+  
+ // Assign the api key (required)
+  config.api_key = API_KEY;
+
+  // Assign the user sign in credentials
+  auth.user.email = USER_EMAIL;
+  auth.user.password = USER_PASSWORD;
+
+  // Assign the RTDB URL (required)
+  config.database_url = DATABASE_URL;
+
+  Firebase.reconnectWiFi(true);
+
+  // Assign the callback function for the long running token generation task */
+  config.token_status_callback = tokenStatusCallback; //see addons/TokenHelper.h
+
+  // Assign the maximum retry of token generation
+  config.max_token_generation_retry = 5;
+
+  // Initialize the library with the Firebase authen and config
+  Firebase.begin(&config, &auth);
+
+  // Streaming (whenever data changes on a path)
+  // Begin stream on a database path --> board1/outputs/digital
+  if (!Firebase.RTDB.beginStream(&stream, listenerPath.c_str()))
+    Serial.printf("stream begin error, %s\n\n", stream.errorReason().c_str());
+
+  // Assign a calback function to run when it detects changes on the database
+  Firebase.RTDB.setStreamCallback(&stream, streamCallback, streamTimeoutCallback);
+
+  // Getting the user UID might take a few seconds
+  Serial.println("Getting User UID");
+  while ((auth.token.uid) == "") {
+    Serial.print('.');
+    delay(1000);
+  }
+  // Print user UID
+  uid = auth.token.uid.c_str();
+  Serial.print("User UID: ");
+  Serial.println(uid);
+
+  // Update database path
+  databasePath = "/UsersData/" + uid;
+
+  // Update database path for sensor readings
+  tempPath = databasePath + "/temperature"; // --> UsersData/<user_uid>/temperature
+  humPath = databasePath + "/humidity"; // --> UsersData/<user_uid>/humidity
+  presPath = databasePath + "/pressure"; // --> UsersData/<user_uid>/pressure
+
+  delay(2000); 
+}
+void loop() {
+  // Check if there are LoRa packets available
+  int packetSize = LoRa.parsePacket();
+  if (packetSize) {
+    getLoRaData();
+    getTimeStamp();
+  }
+  delay(10000);
+
+    // Check if there are LoRa packets available
+ 
+  if (packetSize) {
+    getLoRaData();
+    getTimeStamp();
+
+  }
+  delay(10000); 
+
+   // Check if there are LoRa packets available
+  
+  if (packetSize) {
+    getLoRaData();
+    getTimeStamp();
+  }
+
+   static unsigned long analogSampleTimepoint = millis();
+  if(millis()-analogSampleTimepoint > 40U){     //every 40 milliseconds,read the analog value from the ADC
+    analogSampleTimepoint = millis();
+    analogBuffer[analogBufferIndex] = analogRead(TdsSensorPin);    //read the analog value and store into the buffer
+    analogBufferIndex++;
+    if(analogBufferIndex == SCOUNT){ 
+      analogBufferIndex = 0;
+    }
+    static unsigned long printTimepoint = millis();
+  if(millis()-printTimepoint > 800U){
+    printTimepoint = millis();
+    for(copyIndex=0; copyIndex<SCOUNT; copyIndex++){
+      analogBufferTemp[copyIndex] = analogBuffer[copyIndex];
+      
+      // read the analog value more stable by the median filtering algorithm, and convert to voltage value
+      averageVoltage = getMedianNum(analogBufferTemp,SCOUNT) * (float)VREF / 4096.0;
+      
+      //temperature compensation formula: fFinalResult(25^C) = fFinalResult(current)/(1.0+0.02*(fTP-25.0)); 
+      float compensationCoefficient = 1.0+0.02*(temperature-25.0);
+      //temperature compensation
+      float compensationVoltage=averageVoltage/compensationCoefficient;
+      delay(50);
+  } 
+  // Send new readings to database
+  if (Firebase.ready() && (millis() - sendDataPrevMillis > timerDelay || sendDataPrevMillis == 0)){
+    sendDataPrevMillis = millis();
+    getReadings();
+    sendFloat(tempPath, (pH_Value);
+    sendFloat(humPath, turbidity);
+    sendFloat(presPath, tdsValue);}
+    
+  if (Firebase.isTokenExpired()){
+    Firebase.refreshToken(&config);
+    Serial.println("Refresh token");
+  }
+}
