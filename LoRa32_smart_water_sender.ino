90c112b7a775c83ea766b81f5d7c07bd896920e3
Temalangeni Minenhle Dlamini <152439176+minenhledlamini12@users.noreply.github.com>
2024-03-10 10:59:28 +0200

    Update LoRa32_smart_water_sender.ino

--- LoRa32_smart_water_sender.ino
+++ LoRa32_smart_water_sender.ino
@@ -1,3 +1,7 @@
+/*
+The sender code for uneswa student rural smart water . Aspired by Random Nerd Tutorials.
+*/
+
 //Libraries for LoRa
 #include <SPI.h>
 #include <LoRa.h>
@@ -6,17 +10,7 @@
 #include <Wire.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
-
-#include <Arduino.h>
-#if defined(ESP32)
-  #include <WiFi.h>
-#elif defined(ESP8266)
-  #include <ESP8266WiFi.h>
-#endif
-#include <Firebase_ESP_Client.h>
-#include <Wire.h>
 #include <Adafruit_Sensor.h>
-#include <DHT.h>
 
 //define the pins used by the LoRa transceiver module
 #define SCK 5
@@ -38,52 +32,15 @@
 #define SCREEN_WIDTH 128 // OLED display width, in pixels
 #define SCREEN_HEIGHT 64 // OLED display height, in pixels
 
-// Provide the token generation process info.
-#include "addons/TokenHelper.h"
-// Provide the RTDB payload printing info and other helper functions.
-#include "addons/RTDBHelper.h"
-
-// Insert your network credentials
-#define WIFI_SSID "MTN_4G_B8EB"
-#define WIFI_PASSWORD "30483891"
-
-// Insert Firebase project API Key
-#define API_KEY "AIzaSyCSF_RI5mHbmgcDEPvZHJQ6HRaazeDVros"
-
-// Insert Authorized Username and Corresponding Password
-#define USER_EMAIL "dminenhle477@gmail.com"
-#define USER_PASSWORD "1234567890"
-
+//TDS sensor definition
 #define TdsSensorPin 38
 #define VREF 3.3              // analog reference voltage(Volt) of the ADC
 #define SCOUNT  30            // sum of sample point
 
-// Insert RTDB URLefine the RTDB URL
-#define DATABASE_URL "https://rural-water-project-default-rtdb.firebaseio.com/"
-
-//DHT11 definition
-#define DHTPIN 21
-#define DHTTYPE DHT11
-
+//altrasonic sensor definition
 #define echoPin 35               // CHANGE PIN NUMBER HERE IF YOU WANT TO USE A DIFFERENT PIN
 #define trigPin 34               // CHANGE PIN NUMBER HERE IF YOU WANT TO USE A DIFFERENT PIN
 long duration, distance;
-
-DHT dht(DHTPIN, DHTTYPE);
-
-// Define Firebase objects
-FirebaseData fbdo;
-FirebaseData stream;
-FirebaseAuth auth;
-FirebaseConfig config;
-
-// Variable to save USER UID
-String uid;
-// Variables to save database paths
-String databasePath;
-String tempPath;
-String humPath;
-String presPath;
 
 //pH 
 int pH_Value; 
@@ -91,15 +48,6 @@ float Voltage;
 
 //turbidity
 int turbidity;
-              
-// dht11 sensor
-//Adafruit_BME280 bme; // I2C
-float temperature;
-float humidity;
-
-// Timer variables (send new readings every three minutes)
-unsigned long sendDataPrevMillis = 0;
-unsigned long timerDelay = 5000; //counter delay
 
 // Variables to save database paths
 String listenerPath = "board1/outputs/digital/";
@@ -276,68 +224,6 @@ void sendFloat(String path, float value){
   }
 }
 
-// Callback function that runs on database changes
-void streamCallback(FirebaseStream data){
-  Serial.printf("stream path, %s\nevent path, %s\ndata type, %s\nevent type, %s\n\n",
-                data.streamPath().c_str(),
-                data.dataPath().c_str(),
-                data.dataType().c_str(),
-                data.eventType().c_str());
-  printResult(data); //see addons/RTDBHelper.h
-  Serial.println();
-
-  // Get the path that triggered the function
-  String streamPath = String(data.dataPath());
-
-  // if the data returned is an integer, there was a change on the GPIO state on the following path /{gpio_number}
-  if (data.dataTypeEnum() == fb_esp_rtdb_data_type_integer){
-    String gpio = streamPath.substring(1);
-    int state = data.intData();
-    Serial.print("GPIO: ");
-    Serial.println(gpio);
-    Serial.print("STATE: ");
-    Serial.println(state);
-    digitalWrite(gpio.toInt(), state);
-  }
-
-  /* When it first runs, it is triggered on the root (/) path and returns a JSON with all keys
-  and values of that path. So, we can get all values from the database and updated the GPIO states*/
-  if (data.dataTypeEnum() == fb_esp_rtdb_data_type_json){
-    FirebaseJson json = data.to<FirebaseJson>();
-
-    // To iterate all values in Json object
-    size_t count = json.iteratorBegin();
-    Serial.println("\n---------");
-    for (size_t i = 0; i < count; i++){
-        FirebaseJson::IteratorValue value = json.valueAt(i);
-        int gpio = value.key.toInt();
-        int state = value.value.toInt();
-        Serial.print("STATE: ");
-        Serial.println(state);
-        Serial.print("GPIO:");
-        Serial.println(gpio);
-        digitalWrite(gpio, state);
-        Serial.printf("Name: %s, Value: %s, Type: %s\n", value.key.c_str(), value.value.c_str(), value.type == FirebaseJson::JSON_OBJECT ? "object" : "array");
-    }
-    Serial.println();
-    json.iteratorEnd(); // required for free the used memory in iteration (node data collection)
-  }
-  
-  //This is the size of stream payload received (current and max value)
-  //Max payload size is the payload size under the stream path since the stream connected
-  //and read once and will not update until stream reconnection takes place.
-  //This max value will be zero as no payload received in case of ESP8266 which
-  //BearSSL reserved Rx buffer size is less than the actual stream payload.
-  Serial.printf("Received stream payload size: %d (Max. %d)\n\n", data.payloadLength(), data.maxPayloadLength());
-}
-
-void streamTimeoutCallback(bool timeout){
-  if (timeout)
-    Serial.println("stream timeout, resuming...\n");
-  if (!stream.httpConnected())
-    Serial.printf("error code: %d, reason: %s\n\n", stream.httpCode(), stream.errorReason().c_str());
-}
-
 void setup(){
   Serial.begin(115200);
   startOLED();
@@ -357,61 +243,9 @@ void setup(){
   pinMode(trigPin, OUTPUT);
   pinMode(echoPin, INPUT);
   
-  // Assign the api key (required)
-  config.api_key = API_KEY;
-
-  // Assign the user sign in credentials
-  auth.user.email = USER_EMAIL;
-  auth.user.password = USER_PASSWORD;
-
-  // Assign the RTDB URL (required)
-  config.database_url = DATABASE_URL;
-
-  Firebase.reconnectWiFi(true);
-
-  // Assign the callback function for the long running token generation task */
-  config.token_status_callback = tokenStatusCallback; //see addons/TokenHelper.h
-
-  // Assign the maximum retry of token generation
-  config.max_token_generation_retry = 5;
-
-  // Initialize the library with the Firebase authen and config
-  Firebase.begin(&config, &auth);
-
-  // Streaming (whenever data changes on a path)
-  // Begin stream on a database path --> board1/outputs/digital
-  if (!Firebase.RTDB.beginStream(&stream, listenerPath.c_str()))
-    Serial.printf("stream begin error, %s\n\n", stream.errorReason().c_str());
-
-  // Assign a calback function to run when it detects changes on the database
-  Firebase.RTDB.setStreamCallback(&stream, streamCallback, streamTimeoutCallback);
-
-  // Getting the user UID might take a few seconds
-  Serial.println("Getting User UID");
-  while ((auth.token.uid) == "") {
-    Serial.print('.');
-    delay(1000);
-  }
-  // Print user UID
-  uid = auth.token.uid.c_str();
-  Serial.print("User UID: ");
-  Serial.println(uid);
-
-  // Update database path
-  databasePath = "/UsersData/" + uid;
-
-  // Update database path for sensor readings
-  tempPath = databasePath + "/temperature"; // --> UsersData/<user_uid>/temperature
-  humPath = databasePath + "/humidity"; // --> UsersData/<user_uid>/humidity
-  presPath = databasePath + "/pressure"; // --> UsersData/<user_uid>/pressure
-
-  delay(2000);
-}
 
 void loop(){
 
-  sendReadings();
-  
   delay(50);
    dist();
    if(distance >= 10){
@@ -493,10 +327,6 @@ void loop(){
   }
       //convert voltage value to tds value
       tdsValue=(133.42*compensationVoltage*compensationVoltage*compensationVoltage - 255.86*compensationVoltage*compensationVoltage + 857.39*compensationVoltage)*0.5;
-      
-      //Serial.print("voltage:");
-      //Serial.print(averageVoltage,2);
-      //Serial.print("V   ");
       Serial.print("TDS Value:");
       Serial.print(tdsValue);
       Serial.println("ppm");
@@ -516,16 +346,7 @@ void loop(){
   digitalWrite(Auto, LOW);
   }
   
-   // Send new readings to database
-  if (Firebase.ready() && (millis() - sendDataPrevMillis > timerDelay || sendDataPrevMillis == 0)){
-    sendDataPrevMillis = millis();
-    getReadings();
-    sendFloat(tempPath, (pH_Value);
-    sendFloat(humPath, turbidity);
-    sendFloat(presPath, tdsValue);}
-    
-  if (Firebase.isTokenExpired()){
-    Firebase.refreshToken(&config);
-    Serial.println("Refresh token");
-  }
+      getReadings();
+      sendReadings();
+      delay(10000);
 }
