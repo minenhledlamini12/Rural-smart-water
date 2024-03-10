1d77a1ac7409a66ae36eac88519e1bd00ad7f089
Temalangeni Minenhle Dlamini <152439176+minenhledlamini12@users.noreply.github.com>
2024-03-10 11:02:50 +0200

    Update firebase_rural-smart-water.ino

--- firebase_rural-smart-water.ino
+++ firebase_rural-smart-water.ino
@@ -1,12 +1,11 @@
-/*
-  Complete project details at our blog.
-    - ESP32: https://RandomNerdTutorials.com/firebase-control-esp32-gpios/
-    - ESP8266: https://RandomNerdTutorials.com/firebase-control-esp8266-nodemcu-gpios/
-  Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files.
-  The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-  Based in the RTDB Basic Example by Firebase-ESP-Client library by mobizt
-  https://github.com/mobizt/Firebase-ESP-Client/blob/main/examples/RTDB/Basic/Basic.ino
-*/
+//Libraries for LoRa
+#include <SPI.h>
+#include <LoRa.h>
+
+//Libraries for OLED Display
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
 
 #include <Arduino.h>
 #if defined(ESP32)
@@ -18,6 +17,26 @@
 #include <Wire.h>
 #include <Adafruit_Sensor.h>
 #include <DHT.h>
+
+//define the pins used by the LoRa transceiver module
+#define SCK 5
+#define MISO 19
+#define MOSI 27
+#define SS 18
+#define RST 14
+#define DIO0 26
+
+//433E6 for Asia
+//866E6 for Europe
+//915E6 for North America
+#define BAND 866E6
+
+//OLED pins
+#define OLED_SDA 4
+#define OLED_SCL 15 
+#define OLED_RST 16
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 64 // OLED display height, in pixels
 
 // Provide the token generation process info.
 #include "addons/TokenHelper.h"
@@ -35,7 +54,7 @@
 #define USER_EMAIL "dminenhle477@gmail.com"
 #define USER_PASSWORD "1234567890"
 
-#define TdsSensorPin 27
+#define TdsSensorPin 38
 #define VREF 3.3              // analog reference voltage(Volt) of the ADC
 #define SCOUNT  30            // sum of sample point
 
@@ -45,6 +64,10 @@
 //DHT11 definition
 #define DHTPIN 21
 #define DHTTYPE DHT11
+
+#define echoPin 35               // CHANGE PIN NUMBER HERE IF YOU WANT TO USE A DIFFERENT PIN
+#define trigPin 34               // CHANGE PIN NUMBER HERE IF YOU WANT TO USE A DIFFERENT PIN
+long duration, distance;
 
 DHT dht(DHTPIN, DHTTYPE);
 
@@ -76,7 +99,7 @@ float humidity;
 
 // Timer variables (send new readings every three minutes)
 unsigned long sendDataPrevMillis = 0;
-unsigned long timerDelay = 180000;
+unsigned long timerDelay = 5000; //counter delay
 
 // Variables to save database paths
 String listenerPath = "board1/outputs/digital/";
@@ -84,7 +107,7 @@ String listenerPath = "board1/outputs/digital/";
 // Declare outputs
 const int output1 = 12;
 const int output2 = 13;
-const int output3 = 14;
+const int output3 = 2;
 
 int analogBuffer[SCOUNT];     // store the analog value in the array, read from ADC
 int analogBufferTemp[SCOUNT];
@@ -94,6 +117,14 @@ int copyIndex = 0;
 float averageVoltage = 0;
 float tdsValue = 0;
 float temperaturer = 25;       // current temperature for compensation
+
+String disp;
+const int Auto = 17;
+
+//packet counter
+int readingID = 0;
+int counter = 0;
+String LoRaMessage = "";
 
 // median filtering algorithm
 int getMedianNum(int bArray[], int iFilterLen){
@@ -119,13 +150,101 @@ int getMedianNum(int bArray[], int iFilterLen){
   return bTemp;
 }
 
-void startDHT(){
-  dht.begin();
+void dist(){
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  
+  duration = pulseIn(echoPin, HIGH);
+  distance = duration / 58.2;
+  String disp = String(distance);
+
+  Serial.print("Distance: ");
+  Serial.print(disp);
+  Serial.println(" cm");
+  delay(1000);
+}
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RST);
+
+//Initialize OLED display
+void startOLED(){
+  //reset OLED display via software
+  pinMode(OLED_RST, OUTPUT);
+  digitalWrite(OLED_RST, LOW);
+  delay(20);
+  digitalWrite(OLED_RST, HIGH);
+
+  //initialize OLED
+  Wire.begin(OLED_SDA, OLED_SCL);
+  if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3c, false, false)) { // Address 0x3C for 128x32
+    Serial.println(F("SSD1306 allocation failed"));
+    for(;;); // Don't proceed, loop forever
+  }
+  display.clearDisplay();
+  display.setTextColor(WHITE);
+  display.setTextSize(1);
+  display.setCursor(0,0);
+  display.print("LORA SENDER");
 }
 
-void getReadings(){
-  temperature = dht.readTemperature();
-  humidity = dht.readHumidity();
+//Initialize LoRa module
+void startLoRA(){
+  //SPI LoRa pins
+  SPI.begin(SCK, MISO, MOSI, SS);
+  //setup LoRa transceiver module
+  LoRa.setPins(SS, RST, DIO0);
+
+  while (!LoRa.begin(BAND) && counter < 10) {
+    Serial.print(".");
+    counter++;
+    delay(500);
+  }
+  if (counter == 10) {
+    // Increment readingID on every new reading
+    readingID++;
+    Serial.println("Starting LoRa failed!"); 
+  }
+  Serial.println("LoRa Initialization OK!");
+  display.setCursor(0,10);
+  display.clearDisplay();
+  display.print("LoRa Initializing OK!");
+  display.display();
+  delay(2000);
+}
+
+void sendReadings() {
+  LoRaMessage = String(readingID) + "/" + String(pH_Value) + "&" + String(turbidity) + "#" + String(tdsValue);
+  //Send LoRa packet to receiver
+  LoRa.beginPacket();
+  LoRa.print(LoRaMessage);
+  LoRa.endPacket();
+  
+  display.clearDisplay();
+  display.setCursor(0,0);
+  display.setTextSize(1);
+  display.print("LoRa packet sent!");
+  display.setCursor(0,20);
+  display.print("Turbidity:");
+  display.setCursor(72,20);
+  display.print(turbidity);
+  display.setCursor(0,30);
+  display.print("TDS_Val: ");
+  display.setCursor(54,30);
+  display.print(tdsValue);
+  display.setCursor(0,40);
+  display.print("pH_Val: ");
+  display.setCursor(54,40);
+  display.print(pH_Value);
+  display.setCursor(0,50);
+  display.print("Reading ID:");
+  display.setCursor(66,50);
+  display.print(readingID);
+  display.display();
+  Serial.print("Sending packet: ");
+  Serial.println(readingID);
+  readingID++;
 }
 
 // Initialize WiFi
@@ -221,8 +340,11 @@ void streamTimeoutCallback(bool timeout){
 
 void setup(){
   Serial.begin(115200);
+  startOLED();
+  startDHT();
+  startLoRA();
   initWiFi();
-   startDHT();
+  
    pinMode(TdsSensorPin,INPUT);
    pinMode(pH_Value, INPUT); 
 
@@ -230,7 +352,10 @@ void setup(){
   pinMode(output1, OUTPUT);
   pinMode(output2, OUTPUT);
   pinMode(output3, OUTPUT);
+  pinMode(Auto, OUTPUT);
 
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
   
   // Assign the api key (required)
   config.api_key = API_KEY;
@@ -285,13 +410,47 @@ void setup(){
 
 void loop(){
 
-  pH_Value = analogRead(34); 
+  sendReadings();
+  
+  delay(50);
+   dist();
+   if(distance >= 10){
+    digitalWrite(Auto, HIGH);
+  }
+  if(distance <= 5){
+  digitalWrite(Auto, LOW);
+  }
+  
+  delay(50); 
+ dist();
+ if(distance >= 10){
+    digitalWrite(Auto, HIGH);
+  }
+  if(distance <= 5){
+  digitalWrite(Auto, LOW);
+  }
+
+  pH_Value = analogRead(32); 
   
   Voltage = pH_Value * (3.3 / 4095.0); 
   Serial.println(Voltage); 
   Serial.print("pH: ");
-  Serial.println((pH_Value/585)*2);
-  delay(500);
+  Serial.println((pH_Value);
+  ;
+  delay(50);
+   dist();
+   if(distance >= 10){
+    digitalWrite(Auto, HIGH);
+  }
+  if(distance <= 5){
+  digitalWrite(Auto, LOW);
+  }
+  if(distance >=10){
+    digitalWrite(Auto, HIGH);
+  }
+  if(distance <= 5){
+  digitalWrite(Auto, LOW);
+  }
   
    static unsigned long analogSampleTimepoint = millis();
   if(millis()-analogSampleTimepoint > 40U){     //every 40 milliseconds,read the analog value from the ADC
@@ -301,8 +460,16 @@ void loop(){
     if(analogBufferIndex == SCOUNT){ 
       analogBufferIndex = 0;
     }
-  }   
-  
+  } 
+    
+  delay(50);
+   dist();
+   if(distance >= 10){
+    digitalWrite(Auto, HIGH);
+  }
+  if(distance <= 5){
+  digitalWrite(Auto, LOW);
+  }
   static unsigned long printTimepoint = millis();
   if(millis()-printTimepoint > 800U){
     printTimepoint = millis();
@@ -316,7 +483,14 @@ void loop(){
       float compensationCoefficient = 1.0+0.02*(temperature-25.0);
       //temperature compensation
       float compensationVoltage=averageVoltage/compensationCoefficient;
-      
+      delay(50);
+   dist();
+   if(distance >= 10){
+    digitalWrite(Auto, HIGH);
+  }
+  if(distance <= 5){
+  digitalWrite(Auto, LOW);
+  }
       //convert voltage value to tds value
       tdsValue=(133.42*compensationVoltage*compensationVoltage*compensationVoltage - 255.86*compensationVoltage*compensationVoltage + 857.39*compensationVoltage)*0.5;
       
@@ -324,7 +498,7 @@ void loop(){
       //Serial.print(averageVoltage,2);
       //Serial.print("V   ");
       Serial.print("TDS Value:");
-      Serial.print(tdsValue+70,0);
+      Serial.print(tdsValue);
       Serial.println("ppm");
     }
   }
@@ -332,16 +506,22 @@ void loop(){
   turbidity = analogRead(33); 
 
   Serial.print("Turbidity: ");
-  Serial.println(turbidity+3);
-  delay(500);
+  Serial.println(turbidity);
+  delay(50);
+   dist();
+   if(distance >= 10){
+    digitalWrite(Auto, HIGH);
+  }
+  if(distance <= 5){
+  digitalWrite(Auto, LOW);
+  }
   
    // Send new readings to database
   if (Firebase.ready() && (millis() - sendDataPrevMillis > timerDelay || sendDataPrevMillis == 0)){
     sendDataPrevMillis = millis();
-    getReadings();
-    sendFloat(tempPath, (pH_Value/585)*2);
-    sendFloat(humPath, turbidity+3);
-    sendFloat(presPath, tdsValue+70);}
+    sendFloat(tempPath, (pH_Value);
+    sendFloat(humPath, turbidity);
+    sendFloat(presPath, tdsValue);}
     
   if (Firebase.isTokenExpired()){
     Firebase.refreshToken(&config);
